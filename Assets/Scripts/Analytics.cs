aec83a104966881a58081672d180fad8cbac049b
Xytabich <xytabich@gmail.com>
2020-05-21 09:47:16 +0300

    facebook & game analytics

---
+++ Assets/Scripts/Analytics.cs
@@ -0,0 +1,31 @@
+﻿using Facebook.Unity;
+using GameAnalyticsSDK;
+using UnityEngine;
+
+public class Analytics : MonoBehaviour
+{
+	void Awake()
+	{
+		GameAnalytics.Initialize();
+		FB.Init(FBInitCallback);
+	}
+
+	private void FBInitCallback()
+	{
+		if(FB.IsInitialized)
+		{
+			FB.ActivateApp();
+		}
+	}
+
+	public void OnApplicationPause(bool paused)
+	{
+		if(!paused)
+		{
+			if(FB.IsInitialized)
+			{
+				FB.ActivateApp();
+			}
+		}
+	}
+}
