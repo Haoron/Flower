897aedd89a6ad2b923bffdf87e27352dcced27a1
Haoron <titov.serg.93@gmail.com>
2020-06-18 00:23:10 +0300

    flowercolors and fingertouchoverscreen effect

---
+++ Assets/Scripts/FIngerTouch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FIngerTouch : MonoBehaviour
+{
+    public SpriteRenderer renderer;
+    public Sprite pressed;
+    public Sprite unpressed;
+    public Camera camera;
+    public float distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer.sprite = unpressed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0)) { renderer.sprite = pressed; }
+        else { renderer.sprite = unpressed; }
+        var position = camera.ScreenPointToRay(Input.mousePosition).GetPoint(distance);
+        transform.position = position;
+    }
+}
